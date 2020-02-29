2d828e04ad04869c73c096a8d2ba50397676bf2c
Leo Deg <rdkdvv@gmail.com>
2020-03-01 00:28:50 +0300

    feat: add home page

--- Nature/Nature/Controllers/HomeController.cs
+++ Nature/Nature/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Nature.Models;
+using Nature.Models.ViewModels;
 
 namespace Nature.Controllers
 {
@@ -13,14 +14,32 @@ namespace Nature.Controllers
 	{
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
+		private readonly IRepository<News> _news;
+		private readonly IRepository<Doctor> _doctors;
+		private readonly IRepository<Service> _services;
+
+		public HomeController(ILogger<HomeController> logger, IRepository<Service> services, IRepository<News> news, IRepository<Doctor> doctors)
 		{
 			_logger = logger;
+			_services = services;
+			_news = news;
+			_doctors = doctors;
 		}
 
 		public IActionResult Index()
 		{
-			return View();
+			var doctors = _doctors.Get().OrderByDescending(p => p.Id).Take(3);
+			var services = _services.Get().OrderByDescending(p => p.Id).Take(3);
+			var news = _news.Get().OrderByDescending(p => p.UploadDate).Take(2);
+
+			HomeViewModel model = new HomeViewModel
+			{
+				Doctors = doctors,
+				Services = services,
+				News = news
+			};
+
+			return View(model);
 		}
 
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Nature/Nature/Models/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nature.Models.ViewModels
+{
+	public class HomeViewModel
+	{
+		public IEnumerable<Service> Services { get; set; }
+		public IEnumerable<News> News { get; set; }
+		public IEnumerable<Doctor> Doctors { get; set; }
+
+	}
+}
